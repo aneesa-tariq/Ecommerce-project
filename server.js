92b28fa99bb2272fb9211ee58188398b2f5124d4
Aneesa-Tariq <aneesa.tariq@hotmail.com>
2023-10-16 16:35:14 +0500

    hy

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
-const express = require('express');
+import express from "express";
+import dotenv from "dotenv";
+//configure env,my .env folder is in root folder so we do not need to add path
+dotenv.config();
 
 //rest object
 const app = express();
 //rest api
 app.get('/',(req,res)=>{
-res.send({
-    message:"Welcome To My Ecommerce App"
+res.send('<h1>Welcome to Ecommerce App</h1>');
 })
-})
-//posrt
-const port=8080;
+//port
+const port=process.env.port || 8080;
 //run listen
 app.listen(port,()=>{
-    console.log(`Server running on ${port}`);
+    console.log(`Server running on ${process.env.mode} mode on http://localhost:${port}`);
 })
