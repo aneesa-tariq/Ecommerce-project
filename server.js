b13ecf70588dba753e4f11d897b05c8537ec11be
Aneesa-Tariq <aneesa.tariq@hotmail.com>
2023-10-16 17:30:49 +0500

    "modified"

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 import express from "express";
+import mongoose from 'mongoose';
 import dotenv from "dotenv";
+import morgan from "morgan";
+import connectDB from "./config/db.js";
+
+mongoose.set('strictQuery', true);
 //configure env,my .env folder is in root folder so we do not need to add path
 dotenv.config();
 
 //rest object
 const app = express();
+
+//database config
+connectDB(); 
+
+//middleware
+app.use(express.json);
+app.use(morgan('dev'));
+
 //rest api
 app.get('/',(req,res)=>{
 res.send('<h1>Welcome to Ecommerce App</h1>');
