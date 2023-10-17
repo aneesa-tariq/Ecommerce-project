6c5158524e1dfddc9c88fc30fd433336f2f3aded
Aneesa-Tariq <aneesa.tariq@hotmail.com>
2023-10-17 22:52:01 +0500

    Fix Isues

---
+++ client/src/components/Layout/Layout.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Header from './Header'
+import Footer from './Footer'
+function Layout(props) {
+  return (
+    <div>
+      {/* Header */}
+      <Header/>
+      <main style={{minHeight:'80vh'}}>
+      {props.children}
+      </main>
+      {/* Footer */}
+      <Footer/>
+    </div>
+  )
+}
+
+export default Layout
